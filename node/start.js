d034bb25d31265a4da5444e7440766da3076f2ae
Ryan Olds <ryanrolds@gmail.com>
2011-04-13 16:27:57 -0500

    Coded functional loading, picking, sending of facts. File watch and reloading added.

--- node/start.js
+++ node/start.js
@@ -1,26 +1,57 @@
+var fs = require("fs");
+var lineReader = require("line-reader");
 var mustache = require("mustache");
-var http = require('http');
+
+// Will hold our "facts"
+var facts = [];
+var bullshit = [];
+
+// Load facts
+facts = getFactsFromFile("facts.txt");
+// Load bullshit
+bullshit = getFactsFromFile("bullshit.txt");
+
+// Watch facts and BS files for changes and reload facts on change
+fs.watchFile("facts.txt",function(curr,prev) {
+    if(curr.mtime != prev.mtime) {
+	facts = getFactsFromFile("facts.txt");
+	console.log("Reloading facts");
+    }
+});
+fs.watchFile("bullshit.txt",function(curr,prev) {
+    if(curr.mtime != prev.mtime) {
+	bullshit = getFactsFromFile("bullshit.txt");
+	console.log("Reloading bullshit");
+    }
+});
 
 // TODO: Load template
-var template = "{{say_hello}}, {{name}}";
-// TODO: Load facts
-// TODO: Load bullshit
-// TODO: Setup watchFile on facts and BS
+var template = "{{fact}}";
 
+var http = require('http');
 // Startup server
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'}); // Send headers
+    // Decide if fact or BS
+    var selectedArray = (Math.floor(Math.random()*10) < 9) ? facts : bullshit;
 
-    // TODO: Decide if fact or BS
-    // TODO: Randomly pick line
+    // Randomly pick line
+    var selectedFact = selectedArray[Math.floor(Math.random()*selectedArray.length)];
+    
     // TODO: Create the view
-
-
-    // Testing
-    var view = {name: "Joe", say_hello: function(){ return "hello" }};    
+    var view = {fact: selectedFact};    
     var html = mustache.to_html(template, view);
 
     res.end(html); // Write response
 }).listen(8080);
 
+function getFactsFromFile(file){
+    var incoming = [];
 
+    lineReader.eachLine(file, function(line) {
+	if(line.length > 0)
+	    incoming.push(line)
+    });
+    
+    return incoming;
+}
