fdde83f007bbd559311b890e620f625a261a1b86
Ryan Olds <ryanrolds@gmail.com>
2011-04-18 13:49:20 -0500

    Cleaned up some comparisons

--- node/abtf.js
+++ node/abtf.js
@@ -12,7 +12,8 @@ var factLookup = {};
 var template = ""; // Template
 
 function startHttp() {
-    if(httpServer != undefined || facts.length == 0 || template.length == 0) return;
+    if(httpServer || !facts.length || !template.length) return; // If server already started or facts/template not loaded then return
+    console.log("asdfasdf");
 
     /* The http server */
     httpServer = http.createServer(function (req, res) {
@@ -28,7 +29,7 @@ function startHttp() {
 	    });
 	}
 
-	if(cookies.prev !== undefined) { // If prev is in the cookie
+	if(cookies.prev) { // If prev is in the cookie
 	    cookies.prev.split("|").forEach(function(prevId) { // Get the list from the cookie and turn in to ints
 		prev.push(parseInt(prevId));
 	    });
@@ -39,12 +40,12 @@ function startHttp() {
 	    var urlObj = url.parse(req.url,true);
 
 	    // If /fact and a valid hash then get the fact
-	    if(urlObj.pathname == "/fact" && urlObj.query.hash !== undefined && urlObj.query.hash.length == 32 && factLookup[urlObj.query.hash] !== undefined) 
+	    if(urlObj.pathname == "/fact" && urlObj.query.hash && urlObj.query.hash.length == 32 && factLookup[urlObj.query.hash]) 
 		fact = factLookup[urlObj.query.hash]; // Get that bitch
 	}
 	
 	/* If not a directly linked fact then we randomly get one */
-	if(fact === undefined) { // If a fact hasn't been gotten then get one
+	if(!fact) { // If a fact hasn't been gotten then get one
 	    var f = function(g) { 
 		var fact = facts[Math.floor(Math.random()*facts.length)];
 		return (prev.indexOf(fact.id) == -1) ? fact : g(g);
@@ -88,7 +89,7 @@ function updateFacts() {
 		var fact = false;
 	    }
 
-	    if(fact != false) {
+	    if(fact) {
 		// Post processing
 		fact.hash = hash.md5(fact.id);
 		fact.short = fact.fact.substr(0,130)+((fact.fact.length > 130) ? "...": "");
@@ -105,7 +106,7 @@ function updateFacts() {
 	    factLookup = newFactLookup;
 	    console.log(facts.length+" facts found");
 
-	    if(httpServer == undefined) // If server has been setup then try to start it
+	    if(!httpServer) // If server has been setup then try to start it
 		startHttp();
 	} else {
 	    console.log("No facts found");
@@ -119,7 +120,7 @@ function updateTemplate() {
 
 	template = data;
 
-	if(httpServer == undefined) // If server hasn't been setup
+	if(!httpServer) // If server hasn't been setup
 	    startHttp();
     });
 }
